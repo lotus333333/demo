5f7397c6ead3e20b034cb8931cb6955ffbbd74de
lotus <3550087240@qq.com>
2024-06-27 16:36:19 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//接下来要构建三个静态页面分别是主页、帮助页、关于页。因此我们需要为路由指定好三个不同的 URL
+//直接写控制器名称不行 要写完整的命名空间的位置
+Route::get('/', 'App\Http\Controllers\StaticPagesController@home');
+Route::get('/help', 'App\Http\Controllers\StaticPagesController@help');
+Route::get('/about', 'App\Http\Controllers\StaticPagesController@about');
