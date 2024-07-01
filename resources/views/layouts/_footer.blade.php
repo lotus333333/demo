26732e085fe89da359454778288059c9ddf71752
lotus <3550087240@qq.com>
2024-07-01 15:07:32 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
+        刻意练习，每日精进
+    </a>
+
+    <div class="float-end">
+        <a href="/about" class="text-decoration-none">关于</a>
+    </div>
+</footer>
+<style>
+    footer {
+        margin-top: 45px;
+        padding-top: 5px;
+        border-top: 1px solid #eaeaea;
+        color: #777;
+        font-size: 13px;
+        font-weight: bold;
+
+        a {
+            color: #555;
+        }
+
+        a:hover {
+            color: #222;
+        }
+
+        img.brand-icon {
+            width: 17px;
+            height: 17px;
+        }
+    }
+</style>
