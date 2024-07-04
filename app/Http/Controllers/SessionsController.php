1c7a498e7b4c52853955f27fea8d887d68a86fea
lotus <3550087240@qq.com>
2024-07-04 11:30:37 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,6 +11,7 @@ class SessionsController extends Controller
     {
         return view('sessions.create');
     }
+    //用户登录
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -25,6 +26,13 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+    //退出登录的相关逻辑
+    public  function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出！');
+        return redirect('login');
     }
 }
 
