08ec62c7f581ec2cb910fd6ff95540a05e24ac71
lotus <3550087240@qq.com>
2024-07-06 15:17:48 +0800

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,15 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 //接下来要构建三个静态页面分别是主页、帮助页、关于页。因此我们需要为路由指定好三个不同的 URL
-//直接写控制器名称不行 要写完整的命名空间的位置
+
 Route::get('/', 'App\Http\Controllers\StaticPagesController@home')->name('home');
 Route::get('/help', 'App\Http\Controllers\StaticPagesController@help')->name('help');
 Route::get('/about', 'App\Http\Controllers\StaticPagesController@about')->name('about');
 Route::get('signup','App\Http\Controllers\UsersController@create')->name('signup');
 Route::resource('users','App\Http\Controllers\UsersController');
 
+//显示登录表单页面
 Route::get('login', 'App\Http\Controllers\SessionsController@create')->name('login');
+//处理用户提交的登录表单数据内容
 Route::post('login', 'App\Http\Controllers\SessionsController@store')->name('login');
+//处理用户的退登请求
 Route::delete('logout', 'App\Http\Controllers\SessionsController@destroy')->name('logout');
 
+//用户的激活功能设定的路由
 Route::get('signup/confirm/{token}', 'App\Http\Controllers\UsersController@confirmEmail')->name('confirm_email');
+
+//填写email的表单
+Route::get('password/reset',  'App\Http\Controllers\PasswordController@showLinkRequestForm')->name('password.request');
+//处理表单提交，成功的话就发送邮件，附带token链接
+Route::post('password/email',  'App\Http\Controllers\PasswordController@sendResetLinkEmail')->name('password.email');
+
+//显示更新密码的表单包含token
+Route::get('password/reset/{token}',  'App\Http\Controllers\PasswordController@showResetForm')->name('password.reset');
+//对提交过来的token和email数据进行配对，正确的话就更新密码
+Route::post('password/reset',  'App\Http\Controllers\PasswordController@reset')->name('password.update');
