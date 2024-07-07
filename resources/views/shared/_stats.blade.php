8012fa0f60350a940e47ad27843e7fb2c2f5ffdc
lotus <3550087240@qq.com>
2024-07-07 17:24:57 +0800

    社交统计信息

---
+++ resources/views/shared/_stats.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="#">
+    <strong id="following" class="stat">
+        {{ count($user->followings) }}
+    </strong>
+    关注
+</a>
+<a href="#">
+    <strong id="followers" class="stat">
+        {{ count($user->followers) }}
+    </strong>
+    粉丝
+</a>
+<a href="#">
+    <strong id="statuses" class="stat">
+        {{ $user->statuses()->count() }}
+    </strong>
+    微博
+</a>
+</body>
+</html>
